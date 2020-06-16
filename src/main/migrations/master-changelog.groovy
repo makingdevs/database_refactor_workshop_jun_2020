c8ebd77b498de3c48c3c138e8ae338bd30d3b69c
neodevelop <juan@makingdevs.com>
2020-06-15 20:23:36 -0500

    Setup Liquibase with Gradle and Groovy

---
+++ src/main/migrations/master-changelog.groovy
@@ -0,0 +1,15 @@
+databaseChangeLog {
+  include file: "src/main/migrations/database.changelog.xml"
+  include file: "src/main/migrations/rename-tables.xml"
+  include file: "src/main/migrations/rename-columns.yml"
+  include file: "src/main/migrations/merge-columns.sql"
+  changeSet(author: "neodevelop", id: "202006131124-8") {
+    tagDatabase(tag: "version_1.0")
+  }
+  include file: "src/main/migrations/split-table.xml"
+  changeSet(author: "neodevelop", id: "202006131131-12") {
+    tagDatabase(tag: "version_1.1")
+  }
+  include file: "src/main/migrations/relation-tables.xml"
+  include file: "src/main/migrations/migrate-data.xml"
+}
